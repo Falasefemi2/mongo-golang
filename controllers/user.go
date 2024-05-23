98a8d03c3945d98fe1bc2b6383f424928c1e6a2a
Falasefemi2 <femifalase228@gmail.com>
2024-05-23 09:01:43 +0100

    GetUsers function

--- controllers/user.go
+++ controllers/user.go
@@ -1,1 +1,53 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/falasefemi2/mongo-golang/models"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserController struct {
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController {
+	return &UserController{s}
+}
+
+func (uc UserController) GetUsers(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	u := models.User{}
+
+	err := uc.session.DB("monogo-golang").C("users").FindId(oid).One(&u)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func CreateUser() {}
+
+func DeleteUser() {}
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/falasefemi2/mongo-golang/controllers"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/users/:id", uc.GetUsers)
+	r.POST("/users", uc.CreateUser)
+	r.DELETE("/users/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:271071")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
